5acf125a0aefbd85c1e05963fa628c8f42674c5a
HesiMen <hesi.mendez@gmail.com>
2023-11-20 12:17:17 -0800

    EnemyManager

---
+++ OneButtonBoss/Assets/Scrtips/ScriptableObjectScripts/AIDificultySO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AI Dificulty", menuName = "OneButtonBoss/NewAIDificulty", order = 1)]
+public class AIDificultySO : ScriptableObject
+{
+    // not sure if needed
+}
