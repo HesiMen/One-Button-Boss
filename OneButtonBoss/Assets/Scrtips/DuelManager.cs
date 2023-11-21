c1f2bb2544cb2bd088fdfa77653ed6c9869b2f7a
BlackMambaJesus <negrokagetv@gmail.com>
2023-11-20 20:02:07 -0500

    Player swing being captured via delegate and listened to by DuelManager

--- OneButtonBoss/Assets/Scrtips/DuelManager.cs
+++ OneButtonBoss/Assets/Scrtips/DuelManager.cs
@@ -6,9 +6,22 @@ using UnityEngine;
 public class DuelManager : MonoBehaviour
 {
     public List<EnemyAI> allHeros;
+    [SerializeField] Player player;
+    bool playerAttacking;
 
+    private void Awake()
+    {
+        Player.playerAttack += PlayerAttackFlag;
+    }
 
-
+    private void PlayerAttackFlag()
+    {
+        if (!playerAttacking)
+        {
+            playerAttacking = true;
+        }
+            
+    }
 
     private void Start()
     {
@@ -21,11 +34,27 @@ public class DuelManager : MonoBehaviour
         }
     }
 
+   void Update()
+    {
+        if (playerAttacking)
+        {
+            Debug.Log("Player has attacked");
+            playerAttacking = false;
+        }
+    }
+
+
+
     private void OnAtk(bool atkNow, EnemyAI enemy)
     {
         Debug.Log("Enemy has attacked");
 
+        if (playerAttacking)
+        {
+            Debug.Log("Player has attacked");
+        }
+
         //if killed 
-        enemy.gameObject.SetActive(false);
+        //enemy.gameObject.SetActive(false);
     }
 }
--- OneButtonBoss/Assets/Scrtips/Player.cs
+++ OneButtonBoss/Assets/Scrtips/Player.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Animator playerAnimator; //reference for animator
+    public delegate void OnPlayerAttack();
+    public static OnPlayerAttack playerAttack;
+    bool isPlayerAttacking;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +22,23 @@ public class Player : MonoBehaviour
         Slash();
     }
 
+
     private void Slash()
     {
         if (Input.GetAxis("Slash") > 0f)
         {
+            if (!isPlayerAttacking)
+            {
+                playerAttack.Invoke();
+                isPlayerAttacking = true;
+            }
+                
             playerAnimator.SetBool("isAttacking", true);
         }
         else
         {
+            isPlayerAttacking = false;
+            //playerAttack.Invoke();
             playerAnimator.SetBool("isAttacking", false);
         }
     }
