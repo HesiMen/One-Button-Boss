1cb8086ba3b8dfb39c77f3965744525e69e16d86
HesiMen <hesi.mendez@gmail.com>
2023-11-24 21:08:09 -0800

    Update DuelManager.cs

--- OneButtonBoss/Assets/Scrtips/DuelManager.cs
+++ OneButtonBoss/Assets/Scrtips/DuelManager.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(AudioSource))]
 public class DuelManager : MonoBehaviour
 {
     public List<EnemyAI> allHeros;
+    private EnemyAI currentHero;
     [SerializeField] Player player;
     bool playerAttacking;
     [SerializeField] float timeWindow = 0.5f;
@@ -17,7 +19,7 @@ public class DuelManager : MonoBehaviour
     Quaternion enemyRotation;
     [SerializeField] private FullScreenEffectManager fullScreenEffectManager;
     [SerializeField] private CameraEffectManager cameraEffectManager;
-    
+
     [Header("Audio Settings")]
     [SerializeField] AudioClip clashSound;
     [SerializeField] float clashVolume = 1f;
@@ -28,6 +30,68 @@ public class DuelManager : MonoBehaviour
 
     [SerializeField] AudioSource sfx;
 
+    public bool playerDead = false;
+
+
+    /// Battle Tracker
+    [SerializeField] bool allowEarlyHit = true;
+    private List<Coroutine> activeCoroutines = new List<Coroutine>();
+
+    private float playerAttackTime = -1f; // Time when the player attacked
+    private float enemyAttackTime = -1f; // Time when the enemy attacked
+
+    public UnityEvent wonEvent;
+    public UnityEvent loseEvent;
+    public UnityEvent tooEarlyLoseEvent;
+    public UnityEvent tooEarlyWarmUpEvent;
+    void BattleStarts(System.Func<IEnumerator> currentBattleFunc)
+    {
+        RecordBattle(currentBattleFunc);
+    }
+
+    void RecordBattle(System.Func<IEnumerator> currentBattleFunc)
+    {
+        IEnumerator coroutine = currentBattleFunc.Invoke();
+        Coroutine coroutineReference = StartCoroutine(coroutine);
+        activeCoroutines.Add(coroutineReference);
+    }
+
+    private IEnumerator MyBattle(EnemyAI enemy)
+    {
+        while (!playerDead || enemy.Alive)
+        {
+
+
+            if (!allowEarlyHit && playerAttacking)
+            {
+                Debug.Log("PLayer attacked early - Fail");
+                tooEarlyLoseEvent.Invoke();
+            }
+            else if (allowEarlyHit && playerAttacking)
+            {
+                tooEarlyWarmUpEvent.Invoke();
+                Debug.Log("PLayer attacked early - All Good");
+            }
+
+            
+            yield return null;
+        }
+    }
+
+
+    // Call this method to stop all active coroutines
+    public void StopAllCoroutines()
+    {
+        foreach (var coroutine in activeCoroutines)
+        {
+            StopCoroutine(coroutine);
+        }
+        activeCoroutines.Clear();
+    }
+
+
+
+
     private void Awake()
     {
         Player.playerAttack += PlayerAttackFlag;
@@ -36,6 +100,10 @@ public class DuelManager : MonoBehaviour
 
     private void PlayerAttackFlag()
     {
+        playerAttackTime = Time.time;
+
+        if (currentHero.currentState != EnemyAI.AIState.Attack)
+            return;
         fullScreenEffectManager.PlayAnimateMaterial();
         cameraEffectManager.Zoom();
         cameraEffectManager.ShakeCamera();
@@ -45,55 +113,80 @@ public class DuelManager : MonoBehaviour
 
     public void SubscribeEnemy(EnemyAI enemy)
     {
+        BattleStarts(() => MyBattle(enemy));
+        playerAttackTime = -1f; 
+        enemyAttackTime = -1f; 
+
         fullScreenEffectManager.PlayEngageFight();
         enemy.myAtk += OnAtk;
         player.rotationTarget = enemy.transform;
         allHeros.Add(enemy);
+        currentHero = enemy;
     }
 
 
     void PlayRandomClash()
     {
         float randomPitch = Random.Range(0.85f, 1.15f);
-        sfx.pitch = randomPitch;
-        sfx.PlayOneShot(clashSound, clashVolume);
+        if (sfx != null)
+        {
+            sfx.pitch = randomPitch;
+            sfx.PlayOneShot(clashSound, clashVolume);
+        }
     }
 
 
     private void OnAtk(bool atkNow, EnemyAI enemy)
     {
         Debug.Log("Enemy has attacked");
+        enemyAttackTime = Time.time;
         PlayRandomClash();
         fullScreenEffectManager.PlayExclamationPoint();
         StartCoroutine(SlashWindow(enemy));
 
-        //enemy.transform.position = playerPosition;
-        
-        
-        //if killed 
-        //enemy.myAtk -= OnAtk;
     }
 
 
+
+    private void CheckTimeBeforeEnemyAtk()
+    {
+        if (playerAttackTime < 0 || enemyAttackTime < 0)
+        {
+            Debug.LogError("Attack times not set properly");
+            return;
+        }
+
+        float timeDifference = playerAttackTime - enemyAttackTime;
+        if (timeDifference < 0)
+        {
+            Debug.Log($"Player attacked {Mathf.Abs(timeDifference)} seconds early.");
+        }
+        else
+        {
+            Debug.Log($"Player attacked {timeDifference} seconds late.");
+        }
+
+       
+    }
     /*
      * Timer that controls the amount of time a player 
      * can attack before the battle result is called.
      */
     IEnumerator SlashWindow(EnemyAI enemy)
-    {   
+    {
         // Won't continue Coroutine until player attacks or time window ends
         float currentTime = 0;
-        while(!playerAttacking && currentTime <= timeWindow)
+        while (!playerAttacking && currentTime <= timeWindow)
         {
             currentTime += Time.deltaTime;
             yield return null;
         }
-        
+
         if (playerAttacking)
         {
             //Remove exclamationPoint
             fullScreenEffectManager.PlayRemoveExclamationPoint();
-            
+
             //Swap positions on Attack
             player.transform.position = enemy.transform.position;
             enemy.transform.position = playerPosition;
@@ -110,16 +203,20 @@ public class DuelManager : MonoBehaviour
             fullScreenEffectManager.PlayDisengageFight();
             player.MovePlayerBackToOrigin();
             Debug.Log("Player Won");
+            wonEvent.Invoke();
             enemy.myAtk -= OnAtk;
             playerAttacking = false;
         }
         else
         {
             Debug.Log("Enemy Won");
+            loseEvent.Invoke();
             fullScreenEffectManager.PlayRemoveExclamationPoint();
             AudioSource.PlayClipAtPoint(bloodSound, Camera.main.transform.position, bloodVolume);
             Instantiate(bloodSplatter, player.transform.position, player.transform.rotation);
             player.gameObject.SetActive(false);
         }
+
+        CheckTimeBeforeEnemyAtk();
     }
 }
